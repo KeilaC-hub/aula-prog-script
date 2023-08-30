e698293b62086b37031ea0ce3c79d0fd95c3476f
KEILA CRISTINA DE LIMA RIBEIRO <1090482011049@fatec-srv.local>
2023-08-30 20:21:06 -0300

    aula 03

---
+++ aula3/src/pages/aulaComp.jsx
@@ -0,0 +1,12 @@
+import PrimeiroComponente from "@/components/primeiroComp";
+
+export default function AulaComp(){
+    return(
+        <>
+            <PrimeiroComponente
+                titulo="Meu primeiro componente"
+                subtitulo="Estamos aprendendo NEXT.js com REACT"
+            />
+        </>
+    )
+}
