c1aa1b0bf2ff14b908414d72e6a25a9336401465
KEILA CRISTINA DE LIMA RIBEIRO <1090482011049@fatec-srv.local>
2023-09-11 21:59:33 -0300

    CSS

---
+++ aula3/src/pages/integracao2CSS.jsx
@@ -0,0 +1,12 @@
+import PrimeiroComponente from '@/components/PrimeiroComp'
+import styles from '@/styles/integracao2CSS.module.css'
+
+export default function IntegracaoCSS() {
+    return (
+        <>
+            <h1 className={styles.vermelho}>Teste CSS Module</h1>
+            <h1 className={styles.azul}>Teste CSS Module</h1>
+            <div className='integracao1'><PrimeiroComponente titulo = "Teste com parametro" css = "verde"/> </div>
+        </>
+    )
+}
