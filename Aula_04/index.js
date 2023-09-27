f0f6d66b856ce68eac62fd4985a44702623b590c
KEILA CRISTINA DE LIMA RIBEIRO <keilacris2001clr@gmail.com>
2023-09-27 20:31:13 -0300

    CRUDS

--- Aula_04/index.js
+++ Aula_04/index.js
@@ -7,11 +7,25 @@ app.listen(3000, () => {console.log("O servidor está ativo na porta 3000")});
 
 // CRUD
 // CREATE
-app.post("/postAula", (req, res) => {
-    const {nome, sobrenome} = req.body;
-    console.log(`Os dados são: ${nome} e ${sobrenome}`);
+app.post("/postSorvete", (req, res) => {
+    const {sabor, preco} = req.body;
+    console.log(`Os dados são: ${sabor} e ${preco}`);
+    sabores.push(sabor);
+    precos.push(preco);
     res.send("<h3>Dados inseridos com sucesso!</h3>");
 })
+
 // READ
+app.get("/getSabores", (req, res) => {
+    console.log(sabores);
+    res.send(sabores);
+})
+
 // UPDATE
+app.patch("patchPreco", (req, res) => {
+
+})
 // DELETE
+app.delete("deleteSabor", (req, res) = {
+    
+})
