92a4824fa4f73b0a0c22a78a5105c713319960f1
KEILA CRISTINA DE LIMA RIBEIRO <keilacris2001clr@gmail.com>
2023-10-04 20:19:45 -0300

    POST e GET em json

--- Aula 05-back/index.js
+++ Aula 05-back/index.js
@@ -10,6 +10,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const nodemon = require('nodemon');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
@@ -17,7 +18,21 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log("O servidor está ativo na porta 3000");
 })
-
+// Banco de dados
+let nomes = [];
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/Front/index.html");
+})
+
+app.post("/cadastroChocolate", (req, res) => {
+    const {nome} = req.body
+    // data = "INSERT INTO chocolate (id, nome) VALUES (UUID), ${nome})"
+    nomes.push(nome);
+    res.sendFile(__dirname + "/Front/index.html")
+    console.log(nomes);
+})
+
+app.get("/nomesChocolate", (req, res) => {
+    // res.json(nomes)
+    
 })
